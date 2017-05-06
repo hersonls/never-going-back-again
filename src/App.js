a2d329ba7d2800c22f74b4122435bd821ca2f13c
Samantha Vale <karlasamantha@gmail.com>
2017-05-06 09:42:30 -0300

    removing card and tabs from app.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-
-
-class Tabs extends Component {
-  render() {
-    return (
-      <div className="mdl-layout__tab-bar mdl-js-ripple-effect">
-        <a href="#scroll-tab-1" className="mdl-layout__tab is-active">Tab 1</a>
-        <a href="#scroll-tab-2" className="mdl-layout__tab">Tab 2</a>
-        <a href="#scroll-tab-3" className="mdl-layout__tab">Tab 3</a>
-        <a href="#scroll-tab-4" className="mdl-layout__tab">Tab 4</a>
-        <a href="#scroll-tab-5" className="mdl-layout__tab">Tab 5</a>
-        <a href="#scroll-tab-6" className="mdl-layout__tab">Tab 6</a>
-      </div>
-    );
-  }
-}
-
-
-class Card extends Component {
-  render() {
-    return (
-      <div className="mdl-card mdl-shadow--2dp">
-        <div className="mdl-card__title">
-          <h2 className="mdl-card__title-text">Dia 1</h2>
-        </div>
-        <div className="mdl-card__supporting-text">
-          <ul className="demo-list-three mdl-list">
-            <li className="mdl-list__item mdl-list__item--three-line">
-              <span className="mdl-list__item-primary-content">
-                <i className="material-icons mdl-list__item-avatar">person</i>
-                <span>Talk 1</span>
-                <span className="mdl-list__item-text-body">
-                  Bryan Cranston played the role of Walter in Breaking Bad. He is also known
-                  for playing Hal in Malcom in the Middle.
-                </span>
-              </span>
-              <span className="mdl-list__item-secondary-content">
-                <a className="mdl-list__item-secondary-action" href="#"><i className="material-icons">star</i></a>
-              </span>
-            </li>
-          </ul>
-        </div>
-        <div className="mdl-card__menu">
-          <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-            <i className="material-icons">share</i>
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+import Card from './card/Card';
+import Tabs from './tabs/Tabs';
 
 class Header extends Component {
   render() {
